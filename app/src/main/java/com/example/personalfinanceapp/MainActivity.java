b74a6fdb14408f505c07f0c17e98cf11aab03731
skypekitten9 <victor.enhorning@gmail.com>
2021-05-05 12:59:21 +0200

    SlidePagerAdapter implemented.

--- app/src/main/java/com/example/personalfinanceapp/MainActivity.java
+++ app/src/main/java/com/example/personalfinanceapp/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.personalfinanceapp;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,10 +18,16 @@ import android.widget.Toast;
 import com.example.personalfinanceapp.data.TransactionsEntity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static androidx.fragment.app.FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT;
 
 public class MainActivity extends AppCompatActivity {
     private TransactionsViewModel transactionsViewModel;
+
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
 
 
     @Override
@@ -57,6 +66,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        //Setup pager adapter
+        List<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new Overview());
+        fragmentList.add(new Expenditures());
+        fragmentList.add(new Income());
+
+        viewPager = findViewById(R.id.fragmentPager);
+        pagerAdapter = new SlidePagerAdapter(getSupportFragmentManager(), BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, fragmentList);
+
+        viewPager.setAdapter(pagerAdapter);
     }
 
     public void CreateAccountForm()
